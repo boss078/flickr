b1a1458431efc23f3649c06b5c83e344b0387cab
boss078 <36530564+boss078@users.noreply.github.com>
2020-06-11 15:35:27 +0300

    Added search and responsible design

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const objects = [];
 let photosetsData;
 let carouselData;
+let sortMode = 'default';
+const fullDomObjects = [];
+let domObjects = [];
 $(() => {
   fetch('https://www.flickr.com/services/rest/?method=flickr.collections.getTree&api_key=f6146b5aea320305af01030c6fc04c59&user_id=48600090482%40N01&format=json&nojsoncallback=1')
     .then((response) => response.json())
@@ -18,7 +21,8 @@ $(() => {
       for (let currPageIndex = 0; currPageIndex < Math.ceil(objects.length / 7);
         currPageIndex += 1) {
         const currPage = $('<div></div>')
-          .attr('id', `page${currPageIndex}`);
+          .attr('id', `page${currPageIndex}`)
+          .addClass('pagination-page');
         for (let i = currPageIndex * 7; i < (currPageIndex + 1) * 7 && i < objects.length; i += 1) {
           const objectWrapper = $('<div></div>')
             .addClass('photoset__wrapper')
@@ -29,7 +33,7 @@ $(() => {
             .addClass('photoset__title')
             .text(objects[i].title);
           const objectMore = $('<button>More</button>')
-            .addClass('btn btn-info')
+            .addClass('btn btn-info mb-1')
             .attr('id', 'more-button')
             .attr('type', 'button')
             .attr('onclick', `showPhotosetPhotos(${i})`);
@@ -38,6 +42,8 @@ $(() => {
             .addClass('photoset__description')
             .text(objects[i].description);
           objectWrapper.append(objectHeader, objectDescription);
+          domObjects.push(objectWrapper);
+          fullDomObjects.push(objectWrapper);
           $(currPage).append(objectWrapper);
         }
         currPage.hide();
@@ -102,8 +108,9 @@ function showPhotosetPhotos(photosetIndex) {
             .attr({
               src: `https://farm${element.farm}.staticflickr.com/${element.server}/${element.id}_${element.secret}.jpg`,
               alt: `${element.title}`,
-              width: '766',
-              height: '575',
+              height: 'auto',
+              width: '100%',
+              'max-width': '766',
             });
           const photoTitle = $(`<h3>${element.title}</h3>`);
           const carouselCaption = $('<div></div')
@@ -137,4 +144,77 @@ function showPhotosetPhotos(photosetIndex) {
 function changeCarousel(carouselIndex) {
   $('.carousel-inner').detach();
   $('#carousel-photo').prepend(carouselData[carouselIndex]);
+}
+
+function changeOrder(order) {
+  $('.pagination-page, .page-item').detach();
+  const domObjectsCopy = [...domObjects];
+  switch (order) {
+    default:
+    case 'default':
+      sortMode = 'default';
+      $('.dropdown-toggle').text('Default order');
+      break;
+    case 'ascending':
+      sortMode = 'ascending';
+      $('.dropdown-toggle').text('Alphabetic ascending');
+      domObjectsCopy.sort((a, b) => {
+        if (a[0].firstChild.firstChild.innerText.toLowerCase()
+          < b[0].firstChild.firstChild.innerText.toLowerCase()) {
+          return -1;
+        }
+        if (a[0].firstChild.firstChild.innerText.toLowerCase()
+          > b[0].firstChild.firstChild.innerText.toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      });
+      break;
+    case 'descending':
+      sortMode = 'descending';
+      $('.dropdown-toggle').text('Alphabetic descending');
+      domObjectsCopy.sort((a, b) => {
+        if (a[0].firstChild.firstChild.innerText.toLowerCase()
+          < b[0].firstChild.firstChild.innerText.toLowerCase()) {
+          return 1;
+        }
+        if (a[0].firstChild.firstChild.innerText.toLowerCase()
+          > b[0].firstChild.firstChild.innerText.toLowerCase()) {
+          return -1;
+        }
+        return 0;
+      });
+      break;
+  }
+  for (let currPageIndex = 0; currPageIndex < Math.ceil(domObjectsCopy.length / 7);
+    currPageIndex += 1) {
+    const currPage = $('<div></div>')
+      .attr('id', `page${currPageIndex}`)
+      .addClass('pagination-page');
+    for (let i = currPageIndex * 7; i < (currPageIndex + 1) * 7
+      && i < domObjectsCopy.length; i += 1) {
+      $(currPage).append(domObjectsCopy[i]);
+    }
+    currPage.hide();
+    $('#loaded_data').append(currPage);
+    $('.pagination').append($(`<li class="page-item"><a class="page-link" href="javascript:changePage(${currPageIndex});">${currPageIndex + 1}</a></li>`));
+  }
+  $('.page-item:first').addClass('active');
+  $('#page0').show();
+}
+
+// eslint-disable-next-line no-unused-vars
+function applySearch() {
+  const searchWord = $('#search-field')[0].value.toLowerCase();
+  domObjects = [...fullDomObjects];
+  if (searchWord !== '') {
+    for (let i = 0; i < domObjects.length; i += 1) {
+      if (domObjects[i][0].firstChild.firstChild.innerText.toLowerCase()
+        .search(searchWord) === -1) {
+        domObjects.splice(i, 1);
+        i -= 1;
+      }
+    }
+  }
+  changeOrder(sortMode);
 }
